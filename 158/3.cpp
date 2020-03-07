414c0c7d331a6b7aa1af07835f26306bec318725
o-xian999 <onoyama1999910@gmail.com>
2020-03-07 23:50:03 +0900

    Solve 158-C

--- 158/3.cpp
+++ 158/3.cpp
@@ -9,15 +9,25 @@
 using namespace std;
 
 int main(){
-  double A, B, a, b;
-  //int a,b;
-  cin >> A >> B;
-  a = A / 0.08;
-  b = B / 0.1;
-  int ans = min(a, b);
-  //if(a * 0.02 >= A - B && b * 0.02 >= A - B)cout << -1 << endl;
-  //else if(a < b)cout << a << endl;
-  //else cout << b << endl;
-  if(ans * 0.02 < abs(a - b))cout << -1 << endl;
-  else cout << ans << endl;
+  int a,b;
+  cin >> a >> b;
+  rep(i, 1010){
+    if(int(i * 0.08) == a && int(i * 0.1) == b){
+      cout << i << endl;
+      return 0;
+    }
+  }
+  cout << -1 << endl;
+  return 0;
+  //double A, B, a, b;
+  ////int a,b;
+  //cin >> A >> B;
+  //a = A / 0.08;
+  //b = B / 0.1;
+  //int ans = min(a, b);
+  ////if(a * 0.02 >= A - B && b * 0.02 >= A - B)cout << -1 << endl;
+  ////else if(a < b)cout << a << endl;
+  ////else cout << b << endl;
+  //if(ans * 0.02 < abs(a - b))cout << -1 << endl;
+  //else cout << ans << endl;
 }
