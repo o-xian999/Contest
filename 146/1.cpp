bf9dbe64e6cce9c374391e0bf36e85d8429119c8
o-xian <onoyama1999910@gmail.com>
2020-03-18 22:36:13 +0900

    Solve 146-A

---
+++ 146/1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main (){
+  string S;
+  cin >> S;
+
+  if(S == "SUN") cout << 7 << endl; 
+  if(S == "MON") cout << 6 << endl;
+  if(S == "TUE") cout << 5 << endl;
+  if(S == "WED") cout << 4 << endl;
+  if(S == "THU") cout << 3 << endl;
+  if(S == "FRI") cout << 2 << endl;
+  if(S== "SAT") cout << 1 << endl;
+}
